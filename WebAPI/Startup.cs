0f34506d6a3c4e61d7f8f51539e89288144f9ab7
Huseyin Kan <huseyin.kann@hotmail.com>
2022-12-15 22:31:53 +0300

    newtonson added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -29,7 +29,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddCors();
             //services.AddSingleton<IProductService, ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
